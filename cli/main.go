cadfcdcab2143e074108f7f5009acd30e18c3ac6
Oswaldoooo! <oswaldohome007@gmail.com>
2023-09-10 11:49:22 +0800

    upload mini_im

---
+++ cli/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var add string
+var roomid uint32
+
+const (
+	MSG_SYN = 10
+	MSG_ACK = 11
+	MSG_CTR = 12
+)
+
+var needacceptmsg uint8
+var debuglog = log.New(os.Stderr, "[debug]", log.Ltime|log.Llongfile)
+var output *os.File
+var accept_debug *log.Logger
+
+func init() {
+	flag.StringVar(&add, "add", "127.0.0.1:9000", "--add xxx")
+	var roomidflag uint64
+	flag.Uint64Var(&roomidflag, "roomid", 666, "--roomid xxx")
+	flag.Parse()
+	roomid = uint32(roomidflag)
+	if roomid >= 256*256+256 {
+		debuglog.Println("[error]roomid is too big!!!!range [0~65792]")
+		os.Exit(1)
+	}
+	var err error
+	output, err = os.OpenFile("out.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		debuglog.Println(err.Error())
+		os.Exit(1)
+	}
+	accept_out, err := os.OpenFile("acceptout.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("[error]open accept out failed", err.Error())
+		os.Exit(1)
+	}
+	accept_debug = log.New(accept_out, "[debug]", log.Ltime|log.Llongfile)
+}
+func main() {
+	debuglog.Println("dial to server", add)
+	con, err := net.Dial("tcp", add)
+	if err == nil {
+		var lang int
+		buffer := make([]byte, 1024)
+		_, err = con.Write([]byte{MSG_SYN, byte((roomid / 256) % 256), byte(roomid % 256)})
+		if err != nil {
+			fmt.Println("[error]send connection request to host lost")
+			os.Exit(1)
+		}
+		debuglog.Println("read stattus...")
+		lang, err = con.Read(buffer)
+		if err == nil {
+			if lang == 5 {
+				var (
+					clientid, tiroomid uint32
+				)
+				clientid = uint32(buffer[0])*256*256 + uint32(buffer[1])*256 + uint32(buffer[2])
+				tiroomid = uint32(buffer[3])*256 + uint32(buffer[4])
+				if tiroomid != roomid {
+					fmt.Printf("[error] roomid %v not equal to server roomid %v\n", roomid, tiroomid)
+					os.Exit(1)
+				}
+				go func() {
+					reader := bufio.NewReader(os.Stdin)
+					var content string
+					var msg []byte = make([]byte, 1024)
+					msg[0] = MSG_ACK
+					for {
+						fmt.Printf("[%v %v] ", clientid, roomid)
+						content, err = reader.ReadString('\n')
+						if err == nil {
+							copy(msg[1:], []byte(content))
+							_, err = con.Write(msg[:len(content)+1])
+							if err != nil {
+								debuglog.Println("[error]send msg failed")
+								os.Exit(1)
+							}
+						}
+					}
+				}()
+				//register accept server
+				var (
+					newcon net.Conn
+				)
+				newcon, err = net.Dial("tcp", add)
+				if err != nil {
+					accept_debug.Println("[error] accept server connect to host failed")
+					os.Exit(1)
+				}
+				_, err = newcon.Write([]byte{MSG_SYN, buffer[0], buffer[1], buffer[2]})
+				if err != nil {
+					accept_debug.Println("[error]accept server send connection to host lost", err.Error())
+					os.Exit(1)
+				}
+				accept_debug.Println("[debug] start listen on accept server")
+				for {
+					lang, err = newcon.Read(buffer)
+					if err == nil {
+						switch buffer[0] {
+						case MSG_SYN:
+							msg_syn(buffer[1:lang], newcon)
+						case MSG_ACK:
+							msg_ack(buffer[1:lang], newcon)
+						case MSG_CTR:
+							debuglog.Println(string(buffer[1:lang]))
+						default:
+							accept_debug.Println("[error] not supported msg controller", buffer[0])
+						}
+					} else {
+						if err != io.EOF {
+							accept_debug.Println("[error]", err.Error())
+						} else {
+							accept_debug.Println("server lost connection")
+						}
+						break
+					}
+				}
+			} else {
+				debuglog.Println("Header Msg invalid", lang)
+			}
+		} else {
+			debuglog.Println("[error]", err.Error())
+		}
+	}
+}
+func msg_syn(src []byte, con net.Conn) {
+	needacceptmsg = src[0]
+	fmt.Fprintf(output, "[have %v message to accept]", needacceptmsg)
+}
+func msg_ack(src []byte, con net.Conn) {
+	// debuglog.Println("[from server]", string(src[6:]))
+	fmt.Fprintln(output, "[from server]", string(src[6:]))
+}
---
+++ server/main.go
@@ -0,0 +1,468 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// struct
+type config struct {
+	Port int `yaml:"Port"`
+}
+type client struct {
+	roomid          uint32
+	last_reply_time time.Time
+	last_verion     uint32
+	mutex           sync.RWMutex
+	response_writer *response_Writer
+}
+type response_Writer struct {
+	con  net.Conn
+	done chan struct{}
+}
+type room struct {
+	latestversion uint32
+	mutex         sync.RWMutex
+}
+type message struct {
+	id             int
+	roomid, sendid uint32 //msgid,roomid,send userid
+	content        string
+}
+
+// variables
+const (
+	TIMEOUTMAX = time.Duration(1000) * time.Millisecond
+	MSG_SYN    = 10
+	MSG_ACK    = 11
+	MSG_CTR    = 12
+)
+const (
+	DATA_OUT_OF_RANGE = "data out of range"
+)
+const (
+	QUERY    = 90
+	QUERYROW = 91
+	EXEC     = 92
+)
+
+var (
+	cnf      config
+	errorlog = log.New(os.Stderr, "[error]", log.Ltime|log.Llongfile)
+	debuglog = log.New(os.Stdout, "[debug]", log.Ltime|log.Llongfile)
+	// db         *sql.DB
+	clientlist = make(map[uint32]*client)
+	roomlist   = make(map[uint32]*room)
+	dbmutex    sync.RWMutex //database mutex
+)
+
+func init() {
+	flag.IntVar(&cnf.Port, "Port", 9000, "--cnf.Port xxx")
+	flag.Parse()
+}
+func main() {
+	if cnf.Port < 0 {
+		errorlog.Println("Port", cnf.Port, " is invalid")
+		os.Exit(1)
+	}
+	rand.Seed(time.Now().UnixNano()) //set rand seed
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(cnf.Port))
+	if err == nil {
+		var con net.Conn
+		for {
+			con, err = listener.Accept()
+			if err == nil {
+				go readconse(con)
+			} else {
+				errorlog.Println(err.Error())
+			}
+		}
+	} else {
+		errorlog.Println(err.Error())
+	}
+}
+
+// socket
+func readcon(con net.Conn) {
+	var (
+		lang   int
+		err    error
+		msgarr []message
+	)
+	defer con.Close()
+	buffer := make([]byte, 1500)
+	id := rand.Uint32() % (1 << 24)
+	_, ok := clientlist[id]
+	start := time.Now()
+	fmt.Println("generate rand id", id)
+	for ok && time.Since(start) < TIMEOUTMAX {
+		id = rand.Uint32()
+		_, ok = clientlist[id]
+	}
+	if ok {
+		errorlog.Println("generate id timeout")
+		return
+	}
+	clientlist[id] = &client{}
+	var cli *client = clientlist[id]
+	buffer[0] = byte((id / (256 * 256)) % 256)
+	buffer[1] = byte((id / 256) % 256)
+	buffer[2] = byte(id % 256)
+	fmt.Println("send to clientid to client")
+	_, err = con.Write(buffer[:3])
+	if err == nil {
+		lang, err = con.Read(buffer)
+		if err == nil {
+			if lang == 5 {
+				//parse client id and room
+				var tid uint32 = uint32(buffer[0])*256*256 + uint32(buffer[1])*256 + uint32(buffer[2])
+				if tid == id {
+					_, err = con.Write([]byte{2})
+					if err != nil {
+						errorlog.Println("connection closed")
+						return
+					}
+					var roomid uint32 = uint32(buffer[3])*256 + uint32(buffer[4])
+					fmt.Printf("[debug]roomid origin %v %v\n", buffer[3], buffer[4])
+					if _, ok := roomlist[roomid]; !ok {
+						roomlist[roomid] = &room{}
+						debuglog.Println("create new room", roomid)
+					}
+					cli.roomid = roomid
+					debuglog.Printf("client %v add to room %v", id, roomid)
+					isclose := false
+					go func() {
+						for !isclose {
+							lang, err = con.Read(buffer)
+							if err == nil {
+								if !writetomessage(buffer[1:lang], cli.roomid, id) {
+									_, err = con.Write(writeto(MSG_CTR, "send message failed,unknown error"))
+									if err != nil {
+										isclose = true
+										break
+									}
+								}
+							} else {
+								errorlog.Println("connection closed by client", err.Error())
+								isclose = true
+							}
+						}
+
+					}()
+					for !isclose {
+						cli.mutex.RLock()
+						if roomlist[cli.roomid].latestversion != cli.last_verion {
+							//have new message
+							cli.mutex.RUnlock()
+							cli.mutex.Lock()
+							debuglog.Printf("cli %v roomid %v start get msg", id, cli.roomid)
+							msgarr = getmessage(cli.last_verion, roomid)
+							debuglog.Printf("cli %v get message %d and prepare send to client", id, len(msgarr))
+							err = sendtocli(&msgarr, con)
+							debuglog.Printf("cli %v send message successful", id)
+							cli.last_verion = roomlist[cli.roomid].latestversion
+							cli.mutex.Unlock()
+						} else {
+							cli.mutex.RUnlock()
+						}
+						if err != nil {
+							errorlog.Println("connection closed", err.Error())
+							isclose = true
+						}
+					}
+				} else {
+					con.Write([]byte{1})
+					errorlog.Println("client get wrong clientid,wrong:", tid, "correct:", id)
+				}
+			} else {
+				errorlog.Println("data is not correct", buffer[:lang])
+			}
+		} else if err != io.EOF {
+			errorlog.Println("[error]", err.Error())
+		}
+	} else {
+		errorlog.Println("[error]", err.Error())
+	}
+}
+func readconse(con net.Conn) {
+	defer con.Close()
+
+	buffer := make([]byte, 1024)
+	var (
+		lang      int
+		err       error
+		resp      []byte
+		id        uint32
+		cli       *client
+		is_accept bool = false
+	)
+	defer func() {
+		if !is_accept {
+			debuglog.Println("start deregister main", id, "process")
+		} else {
+			debuglog.Println("start deregister accept", id, "process")
+		}
+		if !is_accept && cli != nil && cli.response_writer != nil {
+			debuglog.Println(id, "send end signal to accept process")
+			cli.response_writer.done <- struct{}{}
+		}
+		if _, ok := clientlist[id]; ok {
+			if !is_accept {
+				debuglog.Println(id, "send server deregistered")
+				delete(clientlist, id)
+			} else {
+				debuglog.Println(id, "accept server deregistered")
+			}
+
+		}
+	}()
+	for {
+		lang, err = con.Read(buffer)
+		if err == nil {
+			switch buffer[0] {
+			case MSG_SYN:
+				if lang == 4 { //注册收信连接
+					is_accept = true
+					id = uint32(buffer[1])*256*256 + uint32(buffer[2])*256 + uint32(buffer[3])
+					var ok bool
+					if cli, ok = clientlist[id]; ok {
+						debuglog.Println("register client", id, "accept server")
+						cli.response_writer = &response_Writer{con: con, done: make(chan struct{})}
+						var msgarr []message
+						tck := time.NewTicker(1 * time.Second)
+						for {
+							select {
+							case <-cli.response_writer.done:
+								debuglog.Println(id, "accept done signal")
+								return
+							case <-tck.C:
+								roomlist[cli.roomid].mutex.RLock()
+								if cli.last_verion < roomlist[cli.roomid].latestversion {
+									debuglog.Printf("client %v find new message", id)
+									roomlist[cli.roomid].mutex.RUnlock()
+									// roomlist[cli.roomid].mutex.Lock()
+									msgarr = getmessage(cli.last_verion, cli.roomid)
+									debuglog.Printf("client %v find %d new messages", id, len(msgarr))
+									if len(msgarr) > 0 {
+										err = sendtocli(&msgarr, con)
+										if err != nil {
+											errorlog.Printf("cli accept server %v closed by client", id)
+											return
+										} else {
+											cli.mutex.Lock()
+											cli.last_verion = roomlist[cli.roomid].latestversion
+											cli.mutex.Unlock()
+											debuglog.Printf("client %v latest version update finished", id)
+										}
+									}
+									// roomlist[cli.roomid].mutex.Unlock()
+								} else {
+									roomlist[cli.roomid].mutex.RUnlock()
+								}
+							}
+						}
+					}
+				} else if lang == 3 { //注册连接
+					id = rand.Uint32() % (256*256*256 + 256*256 + 256)
+					resp = make([]byte, 5)
+					resp[0] = byte((id / (256 * 256)) % 256)
+					resp[1] = byte((id / 256) % 256)
+					resp[2] = byte(id % 256)
+					copy(resp[3:5], buffer[1:3])
+					_, err = con.Write(resp)
+					if err != nil {
+						debuglog.Println("connection closed by client")
+						break
+					}
+					clientlist[id] = &client{roomid: uint32(buffer[1])*256 + uint32(buffer[2]), last_reply_time: time.Now()}
+					cli = clientlist[id]
+					if _, ok := roomlist[cli.roomid]; !ok {
+						roomlist[cli.roomid] = &room{}
+						debuglog.Println("create new room", cli.roomid)
+					}
+					debuglog.Printf("register client %v send server", id)
+				}
+			case MSG_ACK: //客户端向服务端发送信息
+				if !writetomessage(buffer[1:lang], cli.roomid, id) {
+					err = cli.write(MSG_CTR, []byte("send message failed unknown error"))
+					if err != nil {
+						errorlog.Println(err.Error())
+						break
+					}
+				}
+			case MSG_CTR: //切换房间等操作
+
+			}
+		} else {
+			debuglog.Println("client", id, "closed connection")
+			return
+		}
+	}
+}
+func getmessage(version, roomid uint32) []message {
+	var msgarr = []message{}
+	var msg *message
+	debuglog.Println("room list read locked")
+	roomlist[roomid].mutex.RLock()
+	defer roomlist[roomid].mutex.RUnlock()
+	defer fmt.Println("room list read unlocked")
+	rwsany, err := writeTodatabase(QUERY, "select id,sendid,content from `"+strconv.FormatUint(uint64(roomid), 10)+"` where id>?", version)
+	if err == nil {
+		debuglog.Println("write to database finished")
+		rws := rwsany.(*sql.Rows)
+		for rws.Next() {
+			debuglog.Println("read message...")
+			msgarr = append(msgarr, message{roomid: roomid})
+			msg = &msgarr[len(msgarr)-1]
+			err = rws.Scan(&msg.id, &msg.sendid, &msg.content)
+			if err != nil {
+				debuglog.Printf("select id,sendid,content from `"+strconv.FormatUint(uint64(roomid), 10)+"` where id>%v", version)
+				errorlog.Printf("read room %v msg failed,version %v, err %s", roomid, version, err.Error())
+				return msgarr
+			}
+		}
+	}
+	return msgarr
+}
+func sendtocli(src *[]message, con net.Conn) error {
+	if len(*src) == 0 {
+		return nil
+	}
+	_, err := con.Write([]byte{MSG_SYN, byte(len(*src))})
+	if err == nil {
+		debuglog.Println("prepare send", len(*src))
+		for _, ele := range *src {
+			time.Sleep(10 * time.Millisecond) //sleep 10 milliseconds
+			_, err = con.Write(ele.tobytes())
+			if err != nil {
+				return err
+			} else {
+				debuglog.Println("send message", string(ele.tobytes()[7:]))
+			}
+		}
+	}
+	return err
+}
+
+// system level
+// message = id[1]+roomid[2]+sendcliid[3]+content
+func (s *message) tobytes() []byte {
+	ans := make([]byte, 7+len(s.content))
+	ans[0] = MSG_ACK
+	ans[1] = byte(s.id)
+	ans[2] = byte((s.roomid / 256) % 256)
+	ans[3] = byte(s.roomid % 256)
+	ans[4] = byte((s.sendid / (256 * 256)) % 256)
+	ans[5] = byte((s.sendid / 256) % 256)
+	ans[6] = byte(s.sendid % 256)
+	copy(ans[7:], []byte(s.content))
+	return ans
+}
+func (s *client) write(code uint8, v []byte) error {
+	ans := make([]byte, 1+len(v))
+	ans[0] = code
+	copy(ans[1:], v)
+	if s.response_writer == nil {
+		return fmt.Errorf("not set response writer")
+	}
+	_, err := s.response_writer.con.Write(ans)
+	return err
+}
+func writetomessage(message []byte, roomid, sendid uint32) bool {
+	debuglog.Printf("client %v locked room %v", sendid, roomid)
+	roomlist[roomid].mutex.Lock()
+	defer roomlist[roomid].mutex.Unlock()
+	defer debuglog.Printf("client %v unlocked room %v", sendid, roomid)
+	_, err := writeTodatabase(EXEC, "insert into `"+strconv.FormatUint(uint64(roomid), 10)+"` (sendid,content)values(?,?)", sendid, string(message))
+	if err == nil {
+		debuglog.Printf("insert into `%v` (sendid,content)values(%v,'%v')", roomid, sendid, string(message))
+		rowany, _ := writeTodatabase(QUERYROW, "select MAX(id) from `"+strconv.FormatUint(uint64(roomid), 10)+"`")
+		var maxversion uint32
+		row := rowany.(*sql.Row)
+		if row.Scan(&maxversion) == nil {
+			roomlist[roomid].latestversion = maxversion
+			clientlist[sendid].last_verion = maxversion
+		} else {
+			errorlog.Println("get maxversion failed")
+		}
+		return true
+	} else {
+		errorlog.Println("insert data error", err.Error())
+		_, err = writeTodatabase(EXEC, "create table `"+strconv.FormatUint(uint64(roomid), 10)+"` (id INTEGER PRIMARY KEY autoincrement NOT NULL,sendid INTEGER NOT NULL,content VARCHAR(1000))")
+		if err == nil {
+			_, err = writeTodatabase(EXEC, "insert into `"+strconv.FormatUint(uint64(roomid), 10)+"` (sendid,content)values(?,?)", sendid, string(message))
+			if err == nil {
+				rowany, _ := writeTodatabase(QUERYROW, "select MAX(id) from `"+strconv.FormatUint(uint64(roomid), 10)+"`")
+				var maxversion uint32
+				row := rowany.(*sql.Row)
+				if row.Scan(&maxversion) == nil {
+					roomlist[roomid].latestversion = maxversion
+				} else {
+					errorlog.Println("get maxversion failed")
+				}
+				return true
+			} else {
+				errorlog.Println(err.Error())
+			}
+		} else {
+			errorlog.Printf("insert data to room %v failed,err=%s", roomid, err.Error())
+		}
+
+	}
+	return false
+}
+func writeto(code uint8, msg ...string) []byte {
+	var ans []byte
+	if len(msg) == 0 {
+		return []byte{code}
+	}
+	if len(msg[0]) >= 1023 {
+		ans = make([]byte, 1+len(DATA_OUT_OF_RANGE))
+		ans[0] = code
+		copy(ans[1:], []byte(DATA_OUT_OF_RANGE))
+	} else {
+		ans = make([]byte, len(msg[0])+1)
+		ans[0] = code
+		copy(ans[1:], []byte(msg[0]))
+	}
+
+	return ans
+}
+func writeTodatabase(mod uint8, query string, args ...any) (any, error) {
+	defer fmt.Printf("write to database finished,mod %d\n", mod)
+	fmt.Println("start exec sql...")
+	db, err := sql.Open("sqlite3", "data.db")
+	if err == nil {
+		defer db.Close()
+		if mod == QUERY {
+			dbmutex.RLock()
+			defer dbmutex.RUnlock()
+			fmt.Println("do query")
+			return db.QueryContext(context.Background(), query, args...)
+		} else if mod == QUERYROW {
+			dbmutex.RLock()
+			defer dbmutex.RUnlock()
+			fmt.Println("do query row")
+			return db.QueryRowContext(context.Background(), query, args...), nil
+		} else if mod == EXEC {
+			dbmutex.Lock()
+			defer dbmutex.Unlock()
+			fmt.Println("do exec")
+			return db.ExecContext(context.Background(), query, args...)
+		}
+	}
+	return nil, err
+
+}
